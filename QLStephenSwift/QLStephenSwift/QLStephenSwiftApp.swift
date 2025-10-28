95221e9131f59ce234a5032ffaeb3fdb96c5a882
Takashi Mochizuki <mycometg3@icloud.com>
2025-10-29 08:29:50 +0900

    Initial commit: Xcode project with QuickLook Preview Extension

---
+++ QLStephenSwift/QLStephenSwift/QLStephenSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  QLStephenSwiftApp.swift
+//  QLStephenSwift
+//
+//  Created by Takashi Mochizuki on 2025/10/29.
+//
+
+import SwiftUI
+
+@main
+struct QLStephenSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
